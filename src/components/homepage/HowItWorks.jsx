88eb0e53654d4ae4d5d3628d4f0296320760d267
Ayush Kumar <info.ayushkumar@gmail.com>
2024-09-12 21:31:58 +0530

    minor

--- src/components/homepage/HowItWorks.jsx
+++ src/components/homepage/HowItWorks.jsx
@@ -51,7 +51,7 @@ const data = [
     content: (
       <div>
         <p classNameName="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Create your account in minutes. It's free and easy to get started!
+          Create your account in minutes. It&apos;s free and easy to get started!
         </p>
       </div>
     ),
